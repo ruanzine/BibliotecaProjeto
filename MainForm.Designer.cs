8b271de491d18a2023b2367c753715edc0e3fecb
ruanzine <ruan.sena123@outlook.com>
2024-06-25 22:43:09 +0100

    Resize class, Panel Invoker and Menu Panel

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.pnlMainFrame = new System.Windows.Forms.Panel();
             this.pnlLoadForms = new System.Windows.Forms.Panel();
             this.jorge = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
             this.pnlMenu = new System.Windows.Forms.Panel();
-            this.pnlLogo = new System.Windows.Forms.Panel();
-            this.bttStatistics = new System.Windows.Forms.Button();
+            this.bttDateListing = new System.Windows.Forms.Button();
             this.bttListing = new System.Windows.Forms.Button();
             this.bttEdit = new System.Windows.Forms.Button();
             this.bttNew = new System.Windows.Forms.Button();
             this.bttSearch = new System.Windows.Forms.Button();
+            this.pnlLogo = new System.Windows.Forms.Panel();
             this.picLogo = new System.Windows.Forms.PictureBox();
+            this.bttStatistics = new System.Windows.Forms.Button();
             this.pnlMainFrame.SuspendLayout();
             this.jorge.SuspendLayout();
             this.pnlMenu.SuspendLayout();
@@ -56,7 +58,7 @@
             this.pnlMainFrame.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMainFrame.Location = new System.Drawing.Point(0, 0);
             this.pnlMainFrame.Name = "pnlMainFrame";
-            this.pnlMainFrame.Size = new System.Drawing.Size(1226, 580);
+            this.pnlMainFrame.Size = new System.Drawing.Size(1226, 647);
             this.pnlMainFrame.TabIndex = 2;
             // 
             // pnlLoadForms
@@ -65,12 +67,12 @@
             this.pnlLoadForms.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlLoadForms.Location = new System.Drawing.Point(203, 48);
             this.pnlLoadForms.Name = "pnlLoadForms";
-            this.pnlLoadForms.Size = new System.Drawing.Size(1023, 532);
+            this.pnlLoadForms.Size = new System.Drawing.Size(1023, 599);
             this.pnlLoadForms.TabIndex = 4;
             // 
             // jorge
             // 
-            this.jorge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(58)))), ((int)(((byte)(94)))));
+            this.jorge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.jorge.Controls.Add(this.label1);
             this.jorge.Controls.Add(this.lblTitle);
             this.jorge.Dock = System.Windows.Forms.DockStyle.Top;
@@ -84,7 +86,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(183)))), ((int)(((byte)(81)))));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(58)))), ((int)(((byte)(94)))));
             this.label1.Location = new System.Drawing.Point(592, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(418, 23);
@@ -99,14 +101,15 @@
             this.lblTitle.ForeColor = System.Drawing.Color.White;
             this.lblTitle.Location = new System.Drawing.Point(32, 10);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(219, 28);
+            this.lblTitle.Size = new System.Drawing.Size(328, 28);
             this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "LIVRO DE REGISTO";
+            this.lblTitle.Text = "LIVRO DE REGISTOS DIGITAL";
             // 
             // pnlMenu
             // 
             this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(58)))), ((int)(((byte)(94)))));
             this.pnlMenu.Controls.Add(this.bttStatistics);
+            this.pnlMenu.Controls.Add(this.bttDateListing);
             this.pnlMenu.Controls.Add(this.bttListing);
             this.pnlMenu.Controls.Add(this.bttEdit);
             this.pnlMenu.Controls.Add(this.bttNew);
@@ -115,35 +118,27 @@
             this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlMenu.Location = new System.Drawing.Point(0, 0);
             this.pnlMenu.Name = "pnlMenu";
-            this.pnlMenu.Size = new System.Drawing.Size(203, 580);
+            this.pnlMenu.Size = new System.Drawing.Size(203, 647);
             this.pnlMenu.TabIndex = 2;
             // 
-            // pnlLogo
+            // bttDateListing
             // 
-            this.pnlLogo.Controls.Add(this.picLogo);
-            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
-            this.pnlLogo.Name = "pnlLogo";
-            this.pnlLogo.Size = new System.Drawing.Size(203, 152);
-            this.pnlLogo.TabIndex = 1;
-            // 
-            // bttStatistics
-            // 
-            this.bttStatistics.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bttStatistics.Dock = System.Windows.Forms.DockStyle.Top;
-            this.bttStatistics.FlatAppearance.BorderSize = 0;
-            this.bttStatistics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bttStatistics.Font = new System.Drawing.Font("Arial Nova Cond", 12F, System.Drawing.FontStyle.Bold);
-            this.bttStatistics.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.bttStatistics.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.icon_statistics;
-            this.bttStatistics.Location = new System.Drawing.Point(0, 472);
-            this.bttStatistics.Name = "bttStatistics";
-            this.bttStatistics.Size = new System.Drawing.Size(203, 80);
-            this.bttStatistics.TabIndex = 11;
-            this.bttStatistics.Text = "  Relatórios           \r\n";
-            this.bttStatistics.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.bttStatistics.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.bttStatistics.UseVisualStyleBackColor = true;
+            this.bttDateListing.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttDateListing.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bttDateListing.FlatAppearance.BorderSize = 0;
+            this.bttDateListing.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttDateListing.Font = new System.Drawing.Font("Arial Nova Cond", 12F, System.Drawing.FontStyle.Bold);
+            this.bttDateListing.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bttDateListing.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.icon_statistics;
+            this.bttDateListing.Location = new System.Drawing.Point(0, 472);
+            this.bttDateListing.Name = "bttDateListing";
+            this.bttDateListing.Size = new System.Drawing.Size(203, 80);
+            this.bttDateListing.TabIndex = 12;
+            this.bttDateListing.Text = "   Listagem Data  ";
+            this.bttDateListing.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bttDateListing.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.bttDateListing.UseVisualStyleBackColor = true;
+            this.bttDateListing.Click += new System.EventHandler(this.bttDateListing_Click);
             // 
             // bttListing
             // 
@@ -153,12 +148,12 @@
             this.bttListing.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bttListing.Font = new System.Drawing.Font("Arial Nova Cond", 12F, System.Drawing.FontStyle.Bold);
             this.bttListing.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.bttListing.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.icon_list;
+            this.bttListing.Image = ((System.Drawing.Image)(resources.GetObject("bttListing.Image")));
             this.bttListing.Location = new System.Drawing.Point(0, 392);
             this.bttListing.Name = "bttListing";
             this.bttListing.Size = new System.Drawing.Size(203, 80);
-            this.bttListing.TabIndex = 12;
-            this.bttListing.Text = "  Listar Pela Data";
+            this.bttListing.TabIndex = 13;
+            this.bttListing.Text = "   Listagem           ";
             this.bttListing.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.bttListing.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.bttListing.UseVisualStyleBackColor = true;
@@ -172,7 +167,7 @@
             this.bttEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bttEdit.Font = new System.Drawing.Font("Arial Nova Cond", 12F, System.Drawing.FontStyle.Bold);
             this.bttEdit.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.bttEdit.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.icon_editbook;
+            this.bttEdit.Image = ((System.Drawing.Image)(resources.GetObject("bttEdit.Image")));
             this.bttEdit.Location = new System.Drawing.Point(0, 312);
             this.bttEdit.Name = "bttEdit";
             this.bttEdit.Size = new System.Drawing.Size(203, 80);
@@ -191,7 +186,7 @@
             this.bttNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bttNew.Font = new System.Drawing.Font("Arial Nova Cond", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bttNew.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.bttNew.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.icon_newbook;
+            this.bttNew.Image = ((System.Drawing.Image)(resources.GetObject("bttNew.Image")));
             this.bttNew.Location = new System.Drawing.Point(0, 232);
             this.bttNew.Name = "bttNew";
             this.bttNew.Size = new System.Drawing.Size(203, 80);
@@ -210,7 +205,7 @@
             this.bttSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bttSearch.Font = new System.Drawing.Font("Arial Nova Cond", 12F, System.Drawing.FontStyle.Bold);
             this.bttSearch.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.bttSearch.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.icon_searchbook;
+            this.bttSearch.Image = ((System.Drawing.Image)(resources.GetObject("bttSearch.Image")));
             this.bttSearch.Location = new System.Drawing.Point(0, 152);
             this.bttSearch.Name = "bttSearch";
             this.bttSearch.Size = new System.Drawing.Size(203, 80);
@@ -221,11 +216,20 @@
             this.bttSearch.UseVisualStyleBackColor = true;
             this.bttSearch.Click += new System.EventHandler(this.bttSearch_Click);
             // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Controls.Add(this.picLogo);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(203, 152);
+            this.pnlLogo.TabIndex = 1;
+            // 
             // picLogo
             // 
             this.picLogo.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picLogo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.picLogo.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.eb2_3logo;
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
             this.picLogo.Location = new System.Drawing.Point(0, 0);
             this.picLogo.Name = "picLogo";
             this.picLogo.Size = new System.Drawing.Size(203, 152);
@@ -233,15 +237,36 @@
             this.picLogo.TabIndex = 0;
             this.picLogo.TabStop = false;
             // 
+            // bttStatistics
+            // 
+            this.bttStatistics.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttStatistics.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bttStatistics.FlatAppearance.BorderSize = 0;
+            this.bttStatistics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttStatistics.Font = new System.Drawing.Font("Arial Nova Cond", 12F, System.Drawing.FontStyle.Bold);
+            this.bttStatistics.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bttStatistics.Image = global::BIBLIOTECA_PROJETO.Properties.Resources.icon_statistics;
+            this.bttStatistics.Location = new System.Drawing.Point(0, 552);
+            this.bttStatistics.Name = "bttStatistics";
+            this.bttStatistics.Size = new System.Drawing.Size(203, 80);
+            this.bttStatistics.TabIndex = 14;
+            this.bttStatistics.Text = "Estatísticas";
+            this.bttStatistics.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bttStatistics.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.bttStatistics.UseVisualStyleBackColor = true;
+            this.bttStatistics.Click += new System.EventHandler(this.bttStatistics_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1226, 580);
+            this.ClientSize = new System.Drawing.Size(1226, 647);
             this.Controls.Add(this.pnlMainFrame);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Livro de Registos Digital";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.pnlMainFrame.ResumeLayout(false);
@@ -257,8 +282,7 @@
         #endregion
         private System.Windows.Forms.Panel pnlMainFrame;
         private System.Windows.Forms.Panel pnlMenu;
-        private System.Windows.Forms.Button bttListing;
-        private System.Windows.Forms.Button bttStatistics;
+        private System.Windows.Forms.Button bttDateListing;
         private System.Windows.Forms.Button bttSearch;
         private System.Windows.Forms.Button bttEdit;
         private System.Windows.Forms.Button bttNew;
@@ -268,6 +292,8 @@
         private System.Windows.Forms.Panel pnlLoadForms;
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bttListing;
+        private System.Windows.Forms.Button bttStatistics;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -10,83 +10,110 @@ namespace BIBLIOTECA_PROJETO
         private Dictionary<Control, Rectangle> originalControlsBounds = new Dictionary<Control, Rectangle>();
         private Size formOriginalSize;
         private Form _loadForms;
+        private Button _selectedButton;
 
         public MainForm()
         {
             InitializeComponent();
-
+            formOriginalSize = this.Size;
         }
 
-        public void AddControlBounds(Control control, Form form)
+        public void AddControlBounds(Control control)
         {
-            formOriginalSize = ((Form)form).Size;
             foreach (Control childControl in control.Controls)
             {
-                originalControlsBounds.Add(childControl, childControl.Bounds);
-                AddControlBounds(childControl, form);
+                originalControlsBounds[childControl] = new Rectangle(childControl.Location, childControl.Size);
+                AddControlBounds(childControl);
             }
-
-            //foreach (var container in this.Controls.OfType<Panel>())
-            //{
-            //    AddControlBounds((Control)Container, this
-            //}
         }
 
         public void ResizeControls(Control container)
         {
-            float fontSizeRatio = (float)this.Width / 125;
-            if (fontSizeRatio < 8) fontSizeRatio = 8;
+            if (formOriginalSize.Width == 0 || formOriginalSize.Height == 0) return;
+
+            float xRatio = (float)this.Width / formOriginalSize.Width;
+            float yRatio = (float)this.Height / formOriginalSize.Height;
             foreach (Control control in container.Controls)
             {
-                float xRatio = (float)(this.Width) / (float)(formOriginalSize.Width);
-                float yRatio = (float)(this.Height) / (float)(formOriginalSize.Height);
-                int newX = (int)(originalControlsBounds[control].X * xRatio);
-                int newY = (int)(originalControlsBounds[control].Y * yRatio);
-                int newWidth = (int)(originalControlsBounds[control].Width * xRatio);
-                int newHeight = (int)(originalControlsBounds[control].Height * yRatio);
-                control.Location = new Point(newX, newY);
-                control.Size = new Size(newWidth, newHeight);
-                control.Font = new Font(control.Font.FontFamily, fontSizeRatio, control.Font.Style);
+                if (originalControlsBounds.TryGetValue(control, out Rectangle originalBounds))
+                {
+                    int newX = (int)(originalBounds.X * xRatio);
+                    int newY = (int)(originalBounds.Y * yRatio);
+                    int newWidth = (int)(originalBounds.Width * xRatio);
+                    int newHeight = (int)(originalBounds.Height * yRatio);
+                    control.Location = new Point(newX, newY);
+                    control.Size = new Size(newWidth, newHeight);
+                }
                 ResizeControls(control);
-
             }
+        }
 
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            ResizeControls(this);
         }
 
         public void LoadForms(Form form)
         {
             _loadForms?.Close();
-            _loadForms = (Form)form;
+            _loadForms = form;
             _loadForms.TopLevel = false;
             _loadForms.FormBorderStyle = FormBorderStyle.None;
             _loadForms.Dock = DockStyle.Fill;
             pnlLoadForms.Controls.Add(_loadForms);
             _loadForms.Show();
+            AddControlBounds(_loadForms);
+        }
+
+        private void SetSelectedButton(Button button)
+        {
+            if (_selectedButton != null)
+            {
+                _selectedButton.BackColor = Color.FromArgb(51, 58, 94); // Original color
+            }
+            _selectedButton = button;
+            _selectedButton.BackColor = Color.FromArgb(23, 23, 55); // Color to show it's been selected
         }
 
         private void bttNew_Click(object sender, EventArgs e)
         {
+            SetSelectedButton(bttNew);
             LoadAddLivros();
         }
 
         private void bttEdit_Click(object sender, EventArgs e)
         {
+            SetSelectedButton(bttEdit);
             LoadEditLivros();
         }
 
         private void bttSearch_Click(object sender, EventArgs e)
         {
+            SetSelectedButton(bttSearch);
             LoadViewLivros();
         }
 
         private void bttListing_Click(object sender, EventArgs e)
         {
-            LoadDateLivros();
+            SetSelectedButton(bttListing);
+            LoadListing();
         }
 
+        private void bttDateListing_Click(object sender, EventArgs e)
+        {
+            SetSelectedButton(bttDateListing);
+            LoadDateLivros();
+        }
+        private void bttStatistics_Click(object sender, EventArgs e)
+        {
+             SetSelectedButton(bttStatistics);
+            
+        }
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            SetSelectedButton(bttSearch);
+            LoadViewLivros();
         }
 
         public void LoadAddLivros() => LoadForms(new gui.frmAddLivros());
@@ -97,6 +124,7 @@ namespace BIBLIOTECA_PROJETO
 
         public void LoadDateLivros() => LoadForms(new gui.frmDateFromUntil());
 
+        public void LoadListing() => LoadForms(new gui.frmFilteredListing());
 
         public void MainForm_KeyPress(object sender, KeyPressEventArgs e)
         {
